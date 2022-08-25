ca3fec9ddc7f6cd32a682d037d3acf54a1da5766
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-08-25 18:37:27 +0200

    Implement ability to get TEXML entry vlaues

--- texml2ttxt.py
+++ texml2ttxt.py
@@ -1,7 +1,12 @@
 import argparse
 
+from tx3g.texml import TeXML
+
 parser = argparse.ArgumentParser(description='Combine TeXML and TXTT files.')
 parser.add_argument('texml_path', type=str, help='the path to your TeXML file')
 parser.add_argument('txtt_path', type=str, help='the path to your input TXTT file')
 
 args = parser.parse_args()
+
+texml = TeXML(args.texml_path)
+print(texml.get_subtitle_entry_values())
